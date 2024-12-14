46a9230cb0ca4c80e85cfd4ca9209e6902a7b53e
Banita Das <banitaranidas8@gmail.com>
2024-12-14 11:54:28 -0500

    this is counter project

---
+++ Context API/Counter_project/src/App.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+// src/App.jsx
+import React from 'react';
+import './App.css';
+import { CounterProvider } from './CounterContext';
+import Counter from './Counter';
+
+function App() {
+    return (
+        <CounterProvider>
+            <div className="App">
+                <Counter />
+            </div>
+        </CounterProvider>
+    );
+}
+
+export default App;
