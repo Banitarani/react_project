5abd5ba95856077893e00b0496d38cb429596497
Banita Das <banitaranidas8@gmail.com>
2024-12-13 23:14:48 -0500

    To-do list task

---
+++ to-do list/list_project/src/App.jsx
@@ -0,0 +1,15 @@
+
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import './App.css';
+import TodoList from './TodoList';
+
+function App() {
+    return (
+        <div className="App">
+            <TodoList />
+        </div>
+    );
+}
+
+export default App;
